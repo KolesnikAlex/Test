411e2a4cd233f2d9c54219d01cf623126bee3603
user <user@192.168.1.188>
2017-08-21 16:05:14 +0300

    first kommit

---
+++ src/test/Test.java
@@ -0,0 +1,30 @@
+package test;
+/**
+ * 
+ */
+import java.util.*;
+import java.io.IOException;
+import java.nio.*;
+
+/**
+ * @author user
+ *
+ */
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
+
+
